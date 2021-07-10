c5deb2fa11df24c949d8b1ee01181a4252470df6
bahmed99 <ahmed.bahri@ensi-uma.tn>
2021-07-10 21:01:03 +0100

    add

--- src/components/Apropos/Stb.js
+++ src/components/Apropos/Stb.js
@@ -13,11 +13,12 @@ function Stb() {
                 <h1>
                     À propos Hackathon STB</h1>
 
-                <p className="txt">À travers cette opportunité, vous auriez la possibilité de développer votre créativité, votre innovation et votre esprit entrepreneurial. Dans une dizaine d'heures, vous seriez accompagnés des experts qui vous guideront tout au long de votre aventure dont laquelle vous trouverez des solutions technologiques qui touchent aux domaines du tourisme médical et de l'agriculture.
-                    Pour plus d'informations, suivez la page de l'événement et ne manquez aucune publication qui vous sera utile.</p>
+                <p className="txt">La STB en tant  qu’acteur fédérateur de la transformation contribue par la proposition de sujets vecteur d’innovation de deux secteurs clés de l’économie tunisienne.<br /> <br />
+                    <p>Sujet 1 : Solution pour structurer et automatiser la relation entre des différents intervenants dans le secteur du tourisme médical.                                  </p>
+                    <p>Sujet 2 : Solution de traçabilité des produits agroalimentaires.</p>  </p>
                 <div className="logoContainer1">
                     <img className="ima " src={require("../../assets/img/partenaires/stb.jpg").default} width="350px" alt="stb" />
-                    <img className="ima Image3" src={require("../../assets/img/logo/EJE + Text.png").default}  alt="eje" />
+                    <img className="ima Image3" src={require("../../assets/img/logo/EJE + Text.png").default} alt="eje" />
                 </div>
                 <div className="btnContainer" >
                     <Link to="/" className="btn-outline" >Accueil</Link>
